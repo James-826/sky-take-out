5d68f639bfe56f0ac2c2f3429285272b6b540764
eddie <doelt222@gmail.com>
2025-10-28 16:21:06 +0800

    购物车添加业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,32 @@
+package com.sky.controller.user;
+
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import com.sky.vo.DishItemVO;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/shoppingCart")
+@Slf4j
+@Api(tags = "C端购物车相关接口")
+public class ShoppingCartController {
+    private final ShoppingCartService shoppingCartService ;
+    public ShoppingCartController(ShoppingCartService  shoppingCartService) {
+        this.shoppingCartService=shoppingCartService;
+    }
+
+    @PostMapping("/add")
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("添加购物车，商品信息为{}",shoppingCartDTO);
+        shoppingCartService.addShoppingCart(shoppingCartDTO);
+        return Result.success();
+
+    }
+}
